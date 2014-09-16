fb4e7b59ea19d402054cb5a3546020ba54aced03
Jay Wilson Jr <info@jaywilsonjr.com>
2014-09-15 23:59:09 -0400

    Reworked jQuery, GET & POST implemented.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,123 @@
-function renderTemplate(templateId, model, container) {
-  var templateString = $(templateId).text();
-  var templateFunction = _.template(templateString);
-  var renderTemplate = templateFunction(model);
-  $(container).append(renderTemplate);
-};
+var $articles = $('#article_list');
+var $title = $('#title');
+var $article = $('#article');
 
+function addArticle(article) {
+  $articles.append('<li><a href="#">'+ article.title +'</a> '+ article.article + '</li>');
+}
 
-$( "#article_form" ).submit(function( event ) {
-  console.log( $( this ).serializeArray() );
-  event.preventDefault();
-});
-
-
-
-
-
-
-
-var url = 'http://tiny-pizza-server.herokuapp.com/collections/jay';
-
-var test = $.ajax(url, {
-  type: 'POST',
-  dataType: 'json', 
-  data: {
-    title: ['This is a test title.'],
-    content: ['This is fake body content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas ipsa maiores dolorum totam cumque ea, illum adipisci iure autem, laboriosam.']
+$.ajax({
+  type: 'GET',
+  url: 'http://tiny-pizza-server.herokuapp.com/collections/funny_reads',
+  success: function(data) {
+    $.each(data, function(i, item) {
+     addArticle(item);
+    });
+  },
+  error: function() {
+    alert('Error loading content, please contact admin');
   }
 });
 
-console.log(test);
+
+
+$('#add_article').on('click', function() {
+
+  var content = {
+    title: $title.val(),
+    article: $article.val(),
+  };
+
+  $.ajax({
+    type: 'POST', 
+    url: 'http://tiny-pizza-server.herokuapp.com/collections/funny_reads',
+    data: content,
+    success: function(newArticle) {
+     addArticle(newArticle);
+     $('#title').val('');
+     $('#article').val('');
+    },
+    error: function() {
+      alert('Error saving article, please contact Super Admin.');
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  var url = 'http://tiny-pizza-server.herokuapp.com/collections/jay';
+
+
+// function renderTemplate(templateId, model, container) {
+//     var templateString = $(templateId).text();
+//     var templateFunction = _.template(templateString);
+//     var renderTemplate = templateFunction(model);
+//     $(container).append(renderTemplate);
+// };
+
+
+// $("#article_form").submit(function(event) {
+//     var data = $(this).serializeArray();
+//     event.preventDefault();
+
+//     // var result = _.reduce(data, function(acum, item) {
+//     //     acum[item.name] = item.value;
+//     //     return acum;
+//     // }, {});
+
+//     console.log("Before AJAX: ",data);
+//     var test = $.ajax(url, {
+//         type: 'POST',
+//         dataType: 'json',
+//         data: data
+//     });
+
+// });
+
+
+
+
+
+
+
+
+
+// // title: ['This is a test title.'],
+// // content: ['This is fake body content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas ipsa maiores dolorum totam cumque ea, illum adipisci iure autem, laboriosam.']
+// // }
+// // });
+
+// // console.log(test);
