c2ff5efd9441628b1e2f9523c1076293cd5d78ef
Jay Wilson Jr <info@jaywilsonjr.com>
2014-09-15 20:39:21 -0400

    Added Underscore templates and functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('The Iron Yard Rocks');
+function renderTemplate(templateId, model, container) {
+  var templateString = $(templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderTemplate = templateFunction(model);
+  $(container).append(renderTemplate);
+};
+
+
+$( "#article_form" ).submit(function( event ) {
+  console.log( $( this ).serializeArray() );
+  event.preventDefault();
+});
+
+
+
+
+
+
+
+var url = 'http://tiny-pizza-server.herokuapp.com/collections/jay';
+
+var test = $.ajax(url, {
+  type: 'POST',
+  dataType: 'json', 
+  data: {
+    title: ['This is a test title.'],
+    content: ['This is fake body content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas ipsa maiores dolorum totam cumque ea, illum adipisci iure autem, laboriosam.']
+  }
+});
+
+console.log(test);
